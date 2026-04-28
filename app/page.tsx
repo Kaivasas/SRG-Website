207cf69f1cec658ffc7b73a78eea2aae9fadec43
Kaivasas <folky2015@gmail.com>
2026-04-28 16:02:08 +0700

    delete:revalidate in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import WorksSection from "./components/home/WorksSection";
 import TestimonialSection from "./components/home/TestimonialSection";
 import CtaSection from "./components/home/CtaSection";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="relative min-h-screen text-gray-900 font-sans">
       <BlurredBackground />
